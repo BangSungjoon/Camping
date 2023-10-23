7ed848f7ad3537df5146d860105f5a7b51034ce7
sungjoon98 <happypigs7.dev@gmail.com>
2023-10-23 11:03:52 +0900

    11/03

--- camping_project/camping_app/urls.py
+++ camping_project/camping_app/urls.py
@@ -5,12 +5,12 @@ from .views import CampImagesDetailView
 urlpatterns = [
     path('', views.index, name='index'),
     path('camping/list/', views.camping_list, name='camping_list'),
-    path('camping/detail/<str:camp_no>/', views.camping_detail, name='camping_detail'),
+    path('camping/camping_detail/<str:camp_no>/', views.camping_detail, name='camping_detail'),
     path('camping/insert/', views.camping_insert, name='camping_form'),
     path('camp_images/<int:pk>/', CampImagesDetailView.as_view(), name='camp_images_detail'),
     path('camping/safety/', views.camping_safety, name='camping_safety'),
-    path('detail/', views.detail, name='detail'),
+    # path('detail/', views.detail, name='detail'),
     path('camping/search/location', views.camping_search_location, name='camping_search_location'),
     path('get_detail_intro/', views.get_detail_intro, name='get_detail_intro'),
-    path('camping/detail/<str:camp_no>/detail_intro.html', views.detail_intro, name='detail_intro'),
+    path('camping/detail_intro/', views.detail_intro, name='detail_intro'),
 ]
