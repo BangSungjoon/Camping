1b236c9655204b4dfbb8314328445d19a22ab9db
sungjoon98 <happypigs7.dev@gmail.com>
2023-10-24 14:47:47 +0900

    10/24 test

--- camping_project/camping_project/settings.py
+++ camping_project/camping_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'camping_app',
     'users_app'
 ]
