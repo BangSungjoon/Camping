f4f96f858b946fa2d8e248f4235f5215fd9449d1
kmy <hatsukoi.dev@gmail.com>
2023-10-26 11:43:54 +0900

    ere

--- camping_project/camping_app/admin.py
+++ camping_project/camping_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CampMemeberInfo, CampInfo
+
+admin.site.register(CampMemeberInfo)
+admin.site.register(CampInfo)
 
 # Register your models here.
