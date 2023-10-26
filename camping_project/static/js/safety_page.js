912826578003f4a8bafa9e10367e4b9734af52c7
psh <shkb8995@gmail.com>
2023-10-26 14:12:00 +0900

    문의하기

---
+++ camping_project/static/js/safety_page.js
@@ -0,0 +1,40 @@
+function openNewWindow(event) {
+    event.preventDefault();
+    window.open('https://www.youtube.com/watch?v=8U7O0Lh6uX0');
+        var scrollX = window.scrollX || window.pageXOffset;
+        var scrollY = window.scrollY || window.pageYOffset;
+        
+        if (newWindow) {
+            newWindow.onload = function() {
+                newWindow.scrollTo(scrollX, scrollY);
+            };
+        }
+    }
+
+function openNewWindow() {
+    window.open('https://www.youtube.com/watch?v=4aru6TnPDfY');
+    return false;
+    }
+
+function openNewWindow() {
+    window.open('https://www.youtube.com/watch?v=gNC-n32JyHI');
+    return false;
+    }
+
+function openNewWindow() {
+    window.open('https://www.youtube.com/watch?v=D2jdnjSEC1A');
+    return false;
+    }
+
+function openNewWindow() {
+    window.open('https://www.youtube.com/watch?v=LV8JnAyGBSA');
+    return false;
+    }
+
+        
+function openNewWindow() {
+    window.open('https://www.youtube.com/watch?v=NQt_Mem-S6Y');
+    return false;
+    }
+
+
